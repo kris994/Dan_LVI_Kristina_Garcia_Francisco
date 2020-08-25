a407f688635aa8cbfc0524f7056b04b592c4e0fc
kris994 <kristipomo@gmail.com>
2020-08-25 08:43:08 +0200

    Initial Commit

---
+++ Dan_LVI_Kristina_Garcia_Francisco/ViewModel/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Dan_LVI_Kristina_Garcia_Francisco.ViewModel
+{
+    /// <summary>
+    /// Base view model that implements the interface INotifyPropertyChanged
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// PropertyChanged
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Change of the property
+        /// </summary>
+        /// <param name="propertyName">property that is being changed</param>
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
